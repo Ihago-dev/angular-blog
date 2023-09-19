65376e9579ea4d34ac8d5a5b42b7f43f45baa22a
ihago <ihago.g3.system@gmail.com>
2023-09-19 05:31:46 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,30 @@
+export const dataFake = [
+  {
+    id: '1',
+    title: 'NOVO HOMEM DE FERRO É ANUNCIADO',
+    description: 'Marvel studios anuncia novo filme do homem de ferro',
+    photoCover:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqFTnUZhP5BoG2JeY2fYo2ypFduthqT_VQNQ&usqp=CAU',
+  },
+  {
+    id: '2',
+    title: 'Novas séries anunciadas no Disney+',
+    description:
+      'Disney+ anuncia as series de Wandavision, Loki, Falcão e o Soldado Invernal',
+    photoCover:
+      'https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg',
+  },
+  {
+    id: '3',
+    title: 'Novo filme do Pantera Negra é anunciado',
+    description: 'Pantera Negra 3 estréia em Junho de 2026',
+    photoCover:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6pMbLp8vKMzwawc-StfKs-s6EwoyhLgRGFQ&usqp=CAU',
+  },
+  {
+    id: '4',
+    title: 'Novidades sobre o próximo filme dos X-Men',
+    description: 'Pantera Negra 3 estréia em Junho de 2026',
+    photoCover: 'https://cinepop.com.br/wp-content/uploads/2020/07/xmen_1.jpg',
+  },
+];
